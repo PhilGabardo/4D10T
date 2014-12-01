384149087a326c5bd584f658ecc451c367d92dbb
PhilGabardo <gabardpj@mcmaster.ca>
2014-11-30 23:38:32 -0500

    Update Winner.java

--- src/Winner.java
+++ src/Winner.java
@@ -43,18 +43,13 @@ public class Winner extends Activity {
 	    new Handler().postDelayed(new Runnable() {
 	    	 
             /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+* Showing loser splash screen with a timer.
+*/
  
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
                 Intent i = new Intent(Winner.this, MainActivity.class);
                 startActivity(i);
- 
-                // close this activity
                 finish();
             }
         }, 2000);
